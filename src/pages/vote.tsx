66be1348bbb7644cd9e1d19b07f6b125642e37d4
Dejnyyy <danslaby@email.cz>
2023-12-13 12:13:18 +0100

    balambabam

---
+++ src/pages/vote.tsx
@@ -0,0 +1,18 @@
+// pages/about.tsx
+import Link from 'next/link';
+
+const Vote: React.FC = () => {
+	return (
+		<div>
+			<main className="flex min-h-screen flex-col text-white items-center justify-center bg-gradient-to-b from-[#000000] to-[#481b48] text-lg cursor-pointer font-mono font-semibold">
+				<Link href="/" className='absolute right-10 top-5'>back</Link>
+				<section>
+					<div>
+						<h1>Vote</h1>
+					</div>
+				</section>
+			</main>
+		</div>
+	);
+};
+export default Vote;
