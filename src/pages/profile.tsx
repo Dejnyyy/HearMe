bba1f6e03a382c999f9d3ec1f8555de0bda5b09e
Dejnyyy <danslaby@email.cz>
2023-12-19 08:14:39 +0100

    takhle lepski

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -9,7 +9,7 @@ const Profile: React.FC = () => {
 
 	return (
 		<div>
-			<main className="flex min-h-screen flex-col text-white items-center justify-center bg-gradient-to-b from-[#000000] to-[#481b48] text-lg cursor-pointer font-mono font-semibold">
+			<main className="flex min-h-screen flex-col text-white items-center justify-center bg-gradient-to-b from-[#000000] to-[#481b48] text-lg  font-mono font-semibold">
 				<Link href="/" className="absolute right-10 top-5">
 					back
 				</Link>
@@ -19,10 +19,12 @@ const Profile: React.FC = () => {
 					</div>
 				</section>
 				<div className="grid gap-x-60 my-5 grid-cols-3">
-					<div><span>Votes</span> - <span>Registered Date</span></div>
-					<div>02</div>
-					<div>03</div>
+					<div>Favourite Artist</div>
+                    <div><span>Votes</span> - <span>Registered Date</span></div>
+					<div>Favourite Album</div>
 				</div>
+                <h2>Activity</h2>
+                <div className="activity w-4/12 h-24 bg-stone-50 rounded-md my-5"></div>
 			</main>
 		</div>
 	);
--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Ranking: React.FC = () => {
 	return (
 		<div>
-			<main className="flex min-h-screen flex-col text-white items-center justify-center bg-gradient-to-b from-[#000000] to-[#481b48] text-lg cursor-pointer font-mono font-semibold">
+			<main className="flex min-h-screen flex-col text-white items-center justify-center bg-gradient-to-b from-[#000000] to-[#481b48] text-lg  font-mono font-semibold">
 				<Link href="/" className='absolute right-10 top-5'>back</Link>
 				<section>
 					<div>
