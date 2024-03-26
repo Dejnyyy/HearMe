a4bb635706c2bdac476a85a2f4b7fb1f6528b001
Dejnyyy <danslaby@email.cz>
2024-03-26 23:57:49 +0100

    vypisujem voty bejbe

---
+++ src/pages/api/getVotes.ts
@@ -0,0 +1,20 @@
+// pages/api/getVotes.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+import { db } from 'lib/prisma';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    try {
+      // Fetch all votes from the database
+      const votes = await db.vote.findMany();
+      console.log('Fetching votes:', votes);
+      // Return the list of votes
+      res.status(200).json(votes);
+    } catch (error) {
+      console.error('Error fetching votes:', error);
+      res.status(500).json({ success: false, error: 'Error fetching votes' });
+    }
+  } else {
+    res.status(405).json({ success: false, error: 'Method Not Allowed' });
+  }
+}
--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,19 +1,50 @@
-// pages/about.tsx
-import Link from 'next/link';
-import HamburgerMenu from "./components/HamburgerMenu";
+import { useState, useEffect } from 'react';
+import HamburgerMenu from './components/HamburgerMenu';
 
 const Calendar: React.FC = () => {
-	return (
-		<div>
-			<HamburgerMenu />
-			<main className="flex min-h-screen flex-col text-white items-center justify-center bg-black text-lg cursor-pointer font-mono font-semibold">
-				<section>
-					<div>
-						<h1>Calendar</h1>
-					</div>
-				</section>
-			</main>
-		</div>
-	);
+  const [votes, setVotes] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchVotes = async () => {
+      try {
+        const response = await fetch('/api/getVotes');
+        if (!response.ok) {
+          throw new Error('Failed to fetch votes');
+        }
+        const votesData = await response.json();
+        setVotes(votesData);
+      } catch (error) {
+        console.error('Error fetching votes:', error);
+      }
+    };
+
+    fetchVotes();
+  }, []);
+
+  return (
+    <div>
+      <HamburgerMenu />
+      <main className="flex min-h-screen flex-col text-white items-center justify-center bg-black text-lg cursor-pointer font-mono font-semibold">
+        <section>
+          <div>
+            <h1>Calendar</h1>
+          </div>
+          <div>
+            <h2>Votes:</h2>
+            <ul>
+              {votes.map((vote, index) => (
+                <li key={index} className='flex flex-row'>
+                  <p>{vote.song} </p>
+                  <p>{vote.voteType}</p>
+                  <p>Artist: {vote.artist}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 };
+
 export default Calendar;
