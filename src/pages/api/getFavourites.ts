24012e829c878035531527d6862a9142f90e284b
Dejnyyy <danslaby@email.cz>
2024-04-17 21:38:10 +0200

    jen rename..

--- src/pages/api/getFavourites.ts
+++ src/pages/api/getFavourites.ts
@@ -7,17 +7,22 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
   if (!session) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
-
+console.log(session);
   try {
     const user = await db.user.findUnique({
       where: { id: session.user.id },
       select: {
         favoriteArtist: true,
+        favoriteAlbum: true, // Ujistěte se, že přidáte i toto pole
       },
     });
-    res.status(200).json(user);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
   } catch (error) {
     console.error('Error fetching user:', error);
-    res.status(500).json({ message: 'Error fetching favorite artist' });
+    res.status(500).json({ message: 'Error fetching user details' });
   }
-}
+}  
