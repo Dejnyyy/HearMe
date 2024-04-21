0b2826e80e741e2cd8b497bfcefdb76677d56496
Dejnyyy <danslaby@email.cz>
2024-04-21 14:29:32 +0200

    jen vysvetlivky

--- src/pages/api/viewFriendRequests.ts
+++ src/pages/api/viewFriendRequests.ts
@@ -1,5 +1,6 @@
 import { db } from 'lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next'
+//to co nekdo posila mne
 
 export default async function handler(
     req: NextApiRequest,
--- src/pages/api/viewPendingRequests.ts
+++ src/pages/api/viewPendingRequests.ts
@@ -1,5 +1,6 @@
 import { db } from 'lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next'
+//to co ja posilam nekomu
 
 export default async function handler(
     req: NextApiRequest,
