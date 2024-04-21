2742280f7d93cae7bcb0583aed19464da3eeb3b5
Dejnyyy <danslaby@email.cz>
2024-04-21 11:04:59 +0200

    ted funguje pridavani pratel

---
+++ src/pages/api/sendFriendRequest.ts
@@ -0,0 +1,25 @@
+import { db } from 'lib/prisma';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { senderId, receiverId } = req.body;
+    try {
+      const newFriendRequest = await db.friendRequest.create({
+        data: {
+          senderId,
+          receiverId,
+          status: 'pending'
+        }
+      });
+      res.status(200).json(newFriendRequest);
+    } catch (error) {
+      res.status(500).json({ error: "Failed to send friend request." });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
--- src/pages/components/Users.tsx
+++ src/pages/components/Users.tsx
@@ -1,33 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import { User } from '@prisma/client';
 import { useSession } from "next-auth/react";
+import { NextRequest } from 'next/server'; 
 
 interface UsersPageProps {
   userList: User[];
   onDeleteUser: (userId: string) => void;
 }
 
-const UsersPage: React.FC<UsersPageProps> = ({ userList: initialUserList, onDeleteUser }) => {
-  const [userList, setUserList] = useState<User[]>(initialUserList);
-  const { data: sessionData } = useSession();
+interface ExtendedUser extends User {
+  requestPending: boolean;
+}
 
+const UsersPage: React.FC<UsersPageProps> = ({ userList: initialUserList, onDeleteUser }) => {
+  const { data: sessionData } = useSession();
+  const [userList, setUserList] = useState<ExtendedUser[]>(initialUserList.map(user => ({
+    ...user,
+    requestPending: false
+  })));
+  
   useEffect(() => {
-    setUserList(initialUserList);
-  }, [initialUserList]);
+    const fetchPendingRequests = async () => {
+      if (sessionData?.user?.id) {
+        try {
+          const response = await fetch(`/api/viewPendingRequests?userId=${sessionData.user.id}`);
+          if (response.ok) {
+            const pendingRequests = await response.json();
+            const updatedUserList = initialUserList.map(user => ({
+              ...user,
+              requestPending: pendingRequests.some(req=> req.receiverId === user.id)
+            }));
+            setUserList(updatedUserList);
+          }
+        } catch (error) {
+          console.error('Failed to fetch pending friend requests:', error);
+        }
+      }
+    };
+
+    fetchPendingRequests();
+  }, [initialUserList, sessionData]);
   
   // Determine if the logged-in user is an admin
   const isLoggedInUserAdmin = userList.find(user => user.id === sessionData?.user.id)?.isAdmin;
 
   // Function to handle the add friend logic
-  const onAddFriend = (userId: string) => {
-    console.log(`Add friend with ID: ${userId}`); // Implement the logic to add a friend here
+  const onAddFriend = async (userId: string) => {
+    console.log(`Add friend with ID: ${userId}`); 
+    console.log(`Logged in user ID: ${sessionData?.user.id}`);
+    
+    const senderId = sessionData?.user.id;
+    
+  try {
+    const response = await fetch('/api/sendFriendRequest', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ senderId, receiverId: userId })
+    });
+
+    if (response.ok) {
+      // You can add more logic here to handle a successful friend request send
+      console.log('Friend request sent successfully');
+    } else {
+      console.log('Failed to send friend request');
+    }
+  } catch (error) {
+    console.error('Error sending friend request:', error as Error);
+  }
   };
   
   return (
     <div>
       <h1 className='text-white font-mono font-semibold text-xl'>User List</h1>
       <ul className='text-white font-mono mb-5 text-lg'>
-        {userList.map(user => (
+        {userList.map(user => (console.log(user.requestPending),
           <div className='flex items-center m-8' key={user.id}>
             <li className='flex-1'>
               {user.name}
@@ -35,15 +83,16 @@ const UsersPage: React.FC<UsersPageProps> = ({ userList: initialUserList, onDele
             {sessionData?.user.id !== user.id && (
               <button
                 className='ml-2 border px-8 bg-white text-black rounded-xl font-mono font-semibold hover:bg-gray-300'
-                onClick={() => onAddFriend(user.id)}>
-                +
+                onClick={() => onAddFriend(user.id)}
+                disabled={user.requestPending}>
+                {user.requestPending ? 'Pending' : 'Add'}
               </button>
             )}
             {isLoggedInUserAdmin && user.isAdmin !== true && (
               <button
                 className='ml-2 border px-8 bg-white text-black rounded-xl font-mono font-semibold hover:bg-gray-300 hover:border-gray-300'
                 onClick={() => onDeleteUser(user.id)}>
-                x
+                Delete
               </button>
             )}
           </div>
