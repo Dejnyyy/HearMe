7ef97b4b116e6092dee43f23791a71db216f0490
Dejnyyy <danslaby@email.cz>
2024-04-24 08:18:14 +0200

    jde mi uz i odmitani requestu

--- src/pages/api/declineFriendRequest.ts
+++ src/pages/api/declineFriendRequest.ts
@@ -1,25 +1,42 @@
-import { getSession } from "next-auth/react";
-import {db} from 'lib/prisma'; // Adjust the import according to your setup
+import { db } from 'lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-    const session = await getSession({ req });
-    if (!session) {
-      return res.status(401).json({ message: 'Unauthorized' });
-    }
-    const { friendRequestId } = req.body;
-    if (!friendRequestId) {
-        return res.status(400).json({ error: "Friend request ID is required." });
-    }
-
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'POST') {
+    const senderId = req.body.senderId; 
+    const receiverId = req.body.receiverId;
     try {
-        const updatedFriendRequest = await db.friendRequest.update({
-            where: { id: friendRequestId },
-            data: { status: 'declined' }
-        });
-        res.status(200).json(updatedFriendRequest);
+      // Find the first pending friend request where the user is the receiver
+      const friendRequest = await db.friendRequest.findFirst({
+        where: {
+            receiverId: receiverId,
+          senderId: senderId,
+          status: 'pending'
+        }
+      })
+      console.log(req.body)
+      console.log(friendRequest);
+
+      if (!friendRequest) {
+        res.status(404).json({ error: "Pending friend request not found." });
+        return;
+      }
+
+      // Update the friend request to 'declined'
+      const declineFriendRequest = await db.friendRequest.update({
+        where: { id: friendRequest.id },
+        data: { status: 'declined' }
+      });
+
+      res.status(200).json(declineFriendRequest);
     } catch (error) {
-        console.error('Failed to decline friend request:', error);
-        res.status(500).json({ error: 'Failed to decline friend request.' });
+      res.status(500).json({ error: "Failed to accept friend request." });
     }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
