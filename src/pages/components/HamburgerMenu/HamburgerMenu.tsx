754d34710fc0db8083c99ce79739a3cf204f4892
Dejnyyy <danslaby@email.cz>
2024-05-17 11:59:20 +0200

    styly

--- src/pages/components/HamburgerMenu/HamburgerMenu.tsx
+++ src/pages/components/HamburgerMenu/HamburgerMenu.tsx
@@ -24,15 +24,15 @@ const HamburgerMenu: React.FC = () => {
       {isOpen && (
         <div className="py-4 px-2 rounded-lg fixed left-2 top-10 bg-gray-800 text-white">
           <ul className="space-y-1">
-            <li><Link href="/"><p className="font-mono font-semibold text-lg">Home</p></Link></li>
-            <li><Link href="/profile"><p className="font-mono font-semibold text-lg">Profile</p></Link></li>
-            <li><Link href="/calendar"><p className="font-mono font-semibold text-lg">Calendar</p></Link></li>
-            <li><Link href="/ranking"><p className="font-mono font-semibold text-lg">Ranking</p></Link></li>
-            <li><Link href="/explore"><p className="font-mono font-semibold text-lg">Explore</p></Link></li>
-            <li><Link href="/vote"><p className="font-mono font-semibold text-lg">Vote</p></Link></li>
-            <li><Link href="/friends"><p className="font-mono font-semibold text-lg">Friends</p></Link></li>
+            <li><Link href="/"><p className="font-mono font-semibold text-lg cursor-pointer">Home</p></Link></li>
+            <li><Link href="/profile"><p className="font-mono font-semibold text-lg cursor-pointer">Profile</p></Link></li>
+            <li><Link href="/calendar"><p className="font-mono font-semibold text-lg cursor-pointer">Calendar</p></Link></li>
+            <li><Link href="/ranking"><p className="font-mono font-semibold text-lg cursor-pointer">Ranking</p></Link></li>
+            <li><Link href="/explore"><p className="font-mono font-semibold text-lg cursor-pointer">Explore</p></Link></li>
+            <li><Link href="/vote"><p className="font-mono font-semibold text-lg cursor-pointer">Vote</p></Link></li>
+            <li><Link href="/friends"><p className="font-mono font-semibold text-lg cursor-pointer">Friends</p></Link></li>
             {isAdmin && (
-              <li><Link href="/admin"><p className="font-mono font-semibold text-lg border-t-2">Admin</p></Link></li>
+              <li><Link href="/admin"><p className="font-mono font-semibold text-lg border-t-2 cursor-pointer">Admin</p></Link></li>
             )}
           </ul>
         </div>
