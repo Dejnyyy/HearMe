c5179eed8989241042b983b7200e6f7d62fb00db
Dejnyyy <danslaby@email.cz>
2024-06-03 18:51:34 +0200

    pridal scopy

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -19,7 +19,7 @@ export default function Spotify<P extends SpotifyProfile>(
     id: "spotify",
     name: "Spotify",
     type: "oauth",
-    authorization: "https://accounts.spotify.com/authorize?scope=user-read-email",
+    authorization: "https://accounts.spotify.com/authorize?scope=user-read-currently-playing,user-read-recently-played,user-top-read,user-read-email,user-read-private,user-library-read,user-library-modify,user-read-playback-state,user-modify-playback-state",
     token: "https://accounts.spotify.com/api/token",
     userinfo: "https://api.spotify.com/v1/me",
     profile(profile: SpotifyProfile) {
