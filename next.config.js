38cbcf8afc6e1c60d988bdeec4b683f080fb8107
Dejnyyy <danslaby@email.cz>
2024-06-03 13:04:26 +0200

    tak

--- next.config.js
+++ next.config.js
@@ -2,13 +2,5 @@ module.exports = {
   images: {
     domains: ['i.scdn.co','platform-lookaside.fbsbx.com'],
   },
-    async rewrites() {
-      return [
-        {
-          source: '/api/vote',
-          destination: 'http://localhost:3001/vote',
-        },
-      ]
-    },
   }
   
