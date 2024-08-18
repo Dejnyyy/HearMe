65081fefeabbe755b542dcdef7fcba5a3e106360
Dejnyyy <danslaby@email.cz>
2024-08-18 18:49:43 +0200

    upravujeme

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -9,32 +9,9 @@ import { useRouter } from "next/router";
 import { LinearGradient } from 'react-text-gradients'
 
 
-const Home: React.FC<{ userList: User[] }> = () => {
+const HomePage: React.FC<{ userList: User[] }> = () => {
   const { data: sessionData } = useSession();
-  const userID = sessionData?.user?.id;
   const router = useRouter();
-
-  const [scrollProgress, setScrollProgress] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.pageYOffset;
-      const docHeight = document.documentElement.scrollHeight - window.innerHeight;
-      const scrolled = (scrollTop / docHeight) * 100;
-
-      setScrollProgress(scrolled);
-
-      if (scrolled >= 100) {
-        router.push("/profile");
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [router]);
 
   return (
     <>
@@ -43,39 +20,10 @@ const Home: React.FC<{ userList: User[] }> = () => {
         <meta name="description" content="Created by Dejny" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main
-        className="flex min-h-screen flex-col items-center justify-start bg-black"
-        style={{
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          paddingTop: "20px", // Add padding to allow space for scrolling
-        }}
-      >
-        <div style={{ height: "500px" }}></div>
-        <CircularLoading progress={188 - (188 * scrollProgress) / 100} />
+      <main>
+       
       </main>
     </>
   );
 };
-
-export const AuthShowcase: React.FC<AuthShowcaseProps> = ({ userID }) => {
-  const { data: sessionData } = useSession();
-  console.log(sessionData);
-  console.log(userID);
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      {sessionData && <div>{/* Content to show when authenticated */}</div>}
-      <button
-        className="mt-10 rounded-full bg-white px-10 py-3 font-mono font-semibold text-black no-underline transition hover:bg-white/50"
-        onClick={
-          sessionData ? () => void signOut() : () => void signIn("spotify")
-        }
-      >
-        {sessionData ? "Sign out" : "Sign in via Spotify"}
-      </button>
-    </div>
-  );
-};
-
-export default Home;
+export default HomePage;
